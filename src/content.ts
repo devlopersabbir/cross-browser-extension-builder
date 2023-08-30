6553816c4503e17a11b66ec9e19d42c89454838d
devlopersabbir <devlopersabbir@gmail.com>
2023-08-31 01:11:44 +0600

    Update Bakcground script and Content Script

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,3 @@
 import browser from "webextension-polyfill";
 
-class Main {
-  public static facebook() {
-    const facebookElement: HTMLDivElement = document.getElementsByClassName(
-      "x1hc1fzr x1unhpq9 x6o7n8i"
-    )[0] as HTMLDivElement;
-
-    if (facebookElement) {
-      facebookElement.style.display = "none";
-    } else {
-      console.log("not found!");
-    }
-  }
-
-  public static instagram() {}
-}
-
-Main.facebook();
+console.log("hello");
